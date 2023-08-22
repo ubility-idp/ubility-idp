dc6765a3b20422ac29648772c70b4f673627f127
Ibrahim Nasri <ibrahimnasri24@ubilityai.com>
2023-08-22 09:57:02 +0300

    fixed jwt token generation

--- Install/ubility-install/pages/api/automation-setup.ts
+++ Install/ubility-install/pages/api/automation-setup.ts
@@ -1,60 +1,12 @@
 import {NextApiRequest, NextApiResponse} from "next";
-import BashExec from "./utils/BashExec";
-import {
-  addEnvVar,
-  finishedStep,
-  notNonEmptyString,
-} from "./utils/helperFunctions";
-
 import * as crypto from "crypto";
+import {addEnvVar, finishedStep} from "./utils/helperFunctions";
 
-function generateToken(secret: string) {
-  try {
-    const JWT_SECRET = secret;
+var jwt = require("jsonwebtoken");
 
-    const expireSeconds = Number(process.env.JWT_EXPIRATION_IN_SECONDS) || 3600;
-    const isSecretBase64Encoded = false;
-    const jwtSecret: string = isSecretBase64Encoded
-      ? Buffer.from(JWT_SECRET, "base64").toString("utf-8")
-      : JWT_SECRET;
-
-    const header = {
-      alg: "HS256",
-      typ: "JWT",
-    };
-
-    const payload = {
-      id: "0",
-      iat: Math.floor(Date.now() / 1000),
-      name: "ubility-backstage-user",
-      role: "client",
-    };
-
-    const headerBase64 = base64UrlEncode(JSON.stringify(header));
-    const payloadBase64 = base64UrlEncode(JSON.stringify(payload));
-    const signedContent = `${headerBase64}.${payloadBase64}`;
-    const signature = base64UrlEncode(
-      createHmacSHA256(signedContent, JWT_SECRET)
-    );
-
-    const token = `${signedContent}.${signature}`;
-
-    return {pass: true, result: {error: false, result: token}};
-  } catch (error) {
-    return {pass: false, result: {error: true, result: error}};
-  }
-}
-
-function base64UrlEncode(input: string): string {
-  return Buffer.from(input)
-    .toString("base64")
-    .replace(/=/g, "")
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_");
-}
-
-function createHmacSHA256(input: string, key: string): string {
-  return crypto.createHmac("sha256", key).update(input).digest("base64");
+function generateJWTSecret() {
+  const secretLength = 32;
+  return crypto.randomBytes(secretLength).toString("hex");
 }
 
 export default async function handler(
@@ -63,18 +15,26 @@ export default async function handler(
 ) {
   const {step_nb} = req.body;
 
-  const secret = (Math.random() + 1).toString(36);
+  const secret = generateJWTSecret();
 
-  const {pass, result} = generateToken(secret);
+  try {
+    const token = jwt.sign({id: "0", name: "OpenOps Default User"}, secret);
 
-  if (pass) {
+    console.log({secret, token});
+
     addEnvVar("AUTOMATION_SECRET_KEY", secret);
-    addEnvVar("AUTOMATION_SERVER_JWT", result.result as string);
-    finishedStep(step_nb);
-  }
+    addEnvVar("AUTOMATION_SERVER_JWT", token);
 
-  res.status(pass ? 200 : 500).json({
-    status: pass ? "pass" : "fail",
-    result: result,
-  });
+    finishedStep(step_nb);
+
+    res.status(200).json({
+      status: "pass",
+      result: {error: false, result: token},
+    });
+  } catch (error) {
+    res.status(200).json({
+      status: "pass",
+      result: {error: true, result: error},
+    });
+  }
 }
