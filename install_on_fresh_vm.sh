c3793e388f5c4a0735d849dac648ef1823150841
Ibrahim Nasri <ibrahimnasri24@ubilityai.com>
2023-09-06 23:25:09 +0300

    test 2

--- install_on_fresh_vm.sh
+++ install_on_fresh_vm.sh
@@ -72,14 +72,15 @@ docker exec "jenkins-lts" ssh -tt -o StrictHostKeyChecking=no github.com
 # clear
 echo
 echo '--------------- Installation Tool Dependencies ---------------'
-# mkdir -p /etc/apt/keyrings
-# curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-# NODE_MAJOR=18
-# echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-# apt-get update -y
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-source ~/.profile
-nvm install 18
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update -y
+apt-get install nodejs
+# curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+# source ~/.profile
+# nvm install 18
 
 cd Install/ubility-install
 npm install
